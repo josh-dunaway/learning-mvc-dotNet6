dcb7215e998d8d0cdee10ae6fea716bf4d140d54
Joshua Dunaway <joshuadunaway23@gmail.com>
2022-08-02 11:22:05 +0200

    Add project files.

---
+++ DunawaySolutionsWeb/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using DunawaySolutionsWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DunawaySolutionsWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+///test
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
